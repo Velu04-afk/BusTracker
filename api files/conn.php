648f3ab35a474f3ca039e9dd8f402480b8c143e4
anju-1312 <113588224+anju-1312@users.noreply.github.com>
2022-09-18 20:55:25 +0530

    Update conn.php

--- api files/conn.php
+++ api files/conn.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:divyasingh.database.windows.net,1433; Database = BusTracker", "Divya", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:divyasingh.database.windows.net,1433; Database = BusTracker", "Divya", "{Devesh-2002}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
